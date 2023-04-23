a2d588b13a5bc8395a034132c72f84eaf9895ff0
ciornecki <ciornecki@icloud.com>
2023-04-23 22:42:37 +0200

    subpages maintenance :0 8))))

--- src/js/wykonczeniowka.js
+++ src/js/wykonczeniowka.js
@@ -1,8 +1,6 @@
-const cardsCnt = document.querySelector('.right-side');
+const cardsCnt = document.querySelector('.right-side')
 
-const producers = [
-	1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
-];
+const producers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]
 const links = [
 	'https://www.dulux.pl',
 	'https://www.sniezka.pl',
@@ -17,23 +15,21 @@ const links = [
 	'https://www.awenta.pl',
 	'https://www.paradyz.com',
 	'https://www.velux.pl',
+	'https://www.altax.pl',
+	'https://www.effector.pl',
+	'https://nobiles.pl',
 	'https://dre.pl/drzwi-malowane/',
 	'https://www.wkret-met.com',
 	'https://majsterpol.pl',
 	'http://odadoz.pl',
-	'http://www.akzonobel.com',
 	'https://www.sadolin.pl',
-	'https://nobiles.pl',
-	'https://www.altax.pl',
-	'https://www.effector.pl',
-];
+	'http://www.akzonobel.com',
+]
 
 const createCards = () => {
-	producers.forEach((el) => {
-		cardsCnt.innerHTML += `  <a href="${
-			links[el - 1]
-		}" target="_blank" class="card card-img${el}"></a>`;
-	});
-};
+	producers.forEach(el => {
+		cardsCnt.innerHTML += `  <a href="${links[el - 1]}" target="_blank" class="card card-img${el}"></a>`
+	})
+}
 
-createCards();
+createCards()
